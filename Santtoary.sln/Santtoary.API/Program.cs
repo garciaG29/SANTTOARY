4cc40141b759e44d6b371b89a29f4f76e856376a
Santiago AtenciaO <jhony.atencia89@gmail.com>
2026-05-02 12:04:33 -0500

    En program.cs se añadieron: Conexion con base de datos SQL Server, Configuración de Identity para autenticación y autorización (Le dice a la aplicación que SeedDb existe y puede ser usado), Ejecuta el Seed al inicio de la aplicación para poblar la base de datos con datos iniciales.
    Se instala Nugget de Sql

--- Santtoary.sln/Santtoary.API/Program.cs
+++ Santtoary.sln/Santtoary.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Santtoary.API.DATA;
+using Santtoary.Shared.Entidades;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,22 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Conexion con base de datos SQL Server.
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders(); // Configuración de Identity para autenticación y autorización.
+
+builder.Services.AddScoped<SeedDb>();// Le dice a la aplicación que SeedDb existe y puede ser usado.
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seed = scope.ServiceProvider.GetRequiredService<SeedDb>();
+    await seed.SeedAsync();
+} // Ejecuta el Seed al inicio de la aplicación para poblar la base de datos con datos iniciales.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
