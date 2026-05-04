ec0b05306fae2c8d3d3fa4264c6aa3e2bb5aea2d
emmanuelcastaneda19 <ecc200619@gmail.com>
2026-05-04 17:10:49 -0500

    Se añade el controlador del diseño

---
+++ Santtoary.sln/Santtoary.API/Controllers/DesignsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Santtoary.API.DATA;
+using Santtoary.Shared.Entidades;
+
+namespace Santtoary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DesignsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DesignsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // 1. GET: Ver todos los diseños del catálogo
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Designs.ToListAsync());
+        }
+
+        // 2. POST: Subir un diseño nuevo al portafolio
+        [HttpPost]
+        public async Task<ActionResult> Post(Design design)
+        {
+            _context.Designs.Add(design);
+            await _context.SaveChangesAsync();
+            return Ok(design);
+        }
+
+        // 3. PUT: Editar información de un diseño (ej. cambiar el precio o la descripción)
+        [HttpPut]
+        public async Task<ActionResult> Put(Design design)
+        {
+            _context.Designs.Update(design);
+            await _context.SaveChangesAsync();
+            return Ok(design);
+        }
+
+        // 4. DELETE: Eliminar un diseño del catálogo
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var design = await _context.Designs.FirstOrDefaultAsync(x => x.Id == id);
+            if (design == null)
+            {
+                return NotFound();
+            }
+
+            _context.Designs.Remove(design);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
